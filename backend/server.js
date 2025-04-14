c2580eec59c4d202787fffbea3fcfa9cfc5d54ce
saurav nit srinagar <sauravkumar19022002@gmail.com>
2025-04-14 18:18:15 +0530

    for deploying our project

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const messageRoutes = require("./routes/messageRoutes")
+const path = require('path');
 
 
 dotenv.config();
@@ -15,13 +16,33 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is Running Sucessfull  ");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is Running Sucessfull  ");
+// });
+
+
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
+
+//.................this for deployment section..................
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
+  
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Sucessfull  ");
+  });
+  
+}
+
+//.................this for deployment section..................
 app.use(notFound);
 app.use(errorHandler);
 
